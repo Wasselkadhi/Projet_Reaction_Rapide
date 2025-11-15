9adb12dcf5ad60d8e234b48d4761895b744c2208
Wassel <wasselkadhi1235@gmail.com>
2025-11-15 14:06:30 +0100

    quelque changement pour ameliorer le design et l'interface

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
- const container = document.getElementById('splashContainer');
+         const container = document.getElementById('splashContainer');
         const colors = [
             '#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', 
             '#98D8C8', '#F7DC6F', '#BB8FCE', '#85C1E9'
         ];
-        
+        //une background animee avec des couleurs qui s'affichent aux positions aleatoires
         function createSplash() {
             const splash = document.createElement('div');
             splash.classList.add('splash');
             
-           
+           //valeur pour une position aleatoire
             const x = Math.random() * 100;
             const y = Math.random() * 100;
             
-            
+            //choix  de size aleatoire
             const size = Math.random() * 200 + 100;
             
-           
+           //choix ce couleur aleatoire
             const color = colors[Math.floor(Math.random() * colors.length)];
             
-           
+           //initialisation de la splash
             splash.style.width = `${size}px`;
             splash.style.height = `${size}px`;
             splash.style.left = `${x}%`;
             splash.style.top = `${y}%`;
             splash.style.backgroundColor = color;
             
-            const duration = 7 + Math.random() * 2;
+            const duration = 7 + Math.random() * 2;//duree
             splash.style.animationDuration = `${duration}s`;
             
             container.appendChild(splash);
@@ -43,20 +43,18 @@
         
          const navButton = document.getElementById('nav');
         
-         
-        const audio = new Audio('60 Mins Happy Music for Playtime - Playtime Songs for Kids & Toddlers [nRtRlmoyfyw].mp3'); // Add your audio file path
-            
- 
-
-navButton.addEventListener('click', function() {
-    this.classList.toggle('active');
+         // activer/pauser de music lors de click sur le boutton
+        const audio = new Audio('60 Mins Happy Music for Playtime - Playtime Songs for Kids & Toddlers [nRtRlmoyfyw].mp3'); 
+        navButton.addEventListener('click', function() {
+            this.classList.toggle('active');
     
-    if (this.classList.contains('active')) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
+            if (this.classList.contains('active')) {
+                audio.play();
+            } else {
+                audio.pause();
+            }   
 });
+        // le son de click pour chaque boutton
         const clickSound = new Audio('Mouse click-sound effect free to use-VEED.mp3');
 
         document.querySelectorAll('button').forEach(button => {
@@ -71,18 +69,34 @@ navButton.addEventListener('click', function() {
         const contentIframe = document.getElementById('contentIframe');
         const backButton = document.getElementById('backButton');
         const actionButtons = document.querySelectorAll('.action-button');
-
+        //des evenement en click pour chaque boutton
         actionButtons.forEach(button => {
             button.addEventListener('click', function() {
-                const iframeSrc = this.getAttribute('data-src');
+                const iframeSrc = this.getAttribute('data-function');
                 
-                contentIframe.src = iframeSrc;
-                
+                if (iframeSrc==='startGame'){
+                    start();
+                }else if (iframeSrc==='affichageScore'){
+                    affScore();
+                }else{
+                    GuideInfo();
+                }
+                // Activattion de  layout
                 container1.classList.add('iframe-active');
             });
         });
-
+        // fonctionnalite de boutton de retour
         backButton.addEventListener('click', function() {
             container1.classList.remove('iframe-active');
+            contentIframe.srcdoc='';
             
         });
+        function start(){
+            contentIframe.srcdoc='<center><h1>Start Game </h1></center>';
+        }
+        function affScore(){
+           contentIframe.srcdoc='<center><h1>Last score </h1></center>'
+        }
+        function GuideInfo(){
+           contentIframe.srcdoc='<center><h1>Guide </h1></center>'
+        }
